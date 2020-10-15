82bed780ce924690e16136559b5af9b6ffd71e3b
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-10-15 13:22:44 +0200

    start work on client tests

--- client/src/utils/__tests__/functions.test.ts
+++ client/src/utils/__tests__/functions.test.ts
@@ -30,10 +30,36 @@ describe("function to capitalise a word", () => {
   test("return the input unaltered if word is already uppercase", () => {
     expect(functions.capitaliseInput("Franco")).toMatch("Franco");
   });
-});
 
-test("function to check a minimum input length", () => {
-  expect(functions.checkMinimumInputLength("test", 2)).toBe(true);
-  expect(functions.checkMinimumInputLength("one", 6)).toBe(false);
-  expect(functions.checkMinimumInputLength("one", 3)).toBe(true);
+  test("function to check a minimum input length", () => {
+    expect(functions.checkMinimumInputLength("test", 2)).toBe(true);
+    expect(functions.checkMinimumInputLength("one", 6)).toBe(false);
+    expect(functions.checkMinimumInputLength("one", 3)).toBe(true);
+  });
+
+  test("cropInput function", () => {
+    expect(functions.cropInput("password", 3)).toMatch("pas");
+    expect(functions.cropInput("veryLongWord withSpaces", 12)).toMatch(
+      "veryLongWord"
+    );
+  });
+
+  test("removeEntryFromArray function", () => {
+    const stringArr = ["one", "two", "three"];
+    const result = functions.removeEntryFromArray(stringArr, "one");
+    expect(result.length).toBe(2);
+    expect(result[0]).toMatch(/two/);
+    expect(result).toEqual(["two", "three"]);
+
+    const numArr = [1, 2, 3];
+    const result2 = functions.removeEntryFromArray(numArr, 1);
+    expect(result2.length).toBe(2);
+    expect(result2[0]).toBe(2);
+    expect(result2).toEqual([2, 3]);
+  });
+
+  test("createScoreComment function", () => {
+    const result = functions.createScoreComment(5, 10);
+    expect(result).toMatch(/😭 You can do better!/);
+  });
 });
